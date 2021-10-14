816eb69a6d398ef7d6327da7e9b1600c496220e7
gustavdlima <gustavdlima@gmail.com>
2021-10-13 22:50:31 -0300

    fix: norm error

--- ft_printf/libft/ft_memcmp.c
+++ ft_printf/libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gusalves <gusalves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 14:56:07 by gusalves          #+#    #+#             */
-/*   Updated: 2021/08/11 23:40:02 by gusalves         ###   ########.fr       */
+/*   Updated: 2021/10/13 22:49:49 by gusalves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_memcmp(const void *b1, const void *b2, size_t len)
 	{
 		if (((unsigned char *)b1)[index] != ((unsigned char *)b2)[index])
 			return (((unsigned char *)b1)[index] -
-						 ((unsigned char *)b2)[index]);
+						((unsigned char *)b2)[index]);
 		index++;
 	}
 	return (0);
